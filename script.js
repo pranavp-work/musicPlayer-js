98b395e844299818ea8795f7a1d911e7a8bcfc54
pranavp-work <pranavp.work@gmail.com>
2024-03-01 19:40:57 +0530

    added controls play/pause functionalities.

---
+++ script.js
@@ -0,0 +1,22 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+let isPlaying = false;
+
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
