46f725c66fa4ef08630489bcc79fcb7eb28297f9
pranavp-work <pranavp.work@gmail.com>
2024-03-05 23:56:07 +0530

    progress bar completed.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -80,5 +82,16 @@ function prevSong() {
 // On Startup Load First Song
 loadSong(songs[songIndex]);
 
+function updateProgressBar(event) {
+    if(isPlaying) {
+        const { duration, currentTime } = event.srcElement;
+        console.log(duration, currentTime);
+
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
